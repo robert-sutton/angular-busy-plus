8f1aa38dd667dd8fe12da6a1732ab3d0c58f6be6
Jeremey Hustman <jeremeyhustman@gmail.com>
2016-05-18 10:44:56 -0800

    add "setting defaults" demo

---
+++ demo/demo-setting-defaults.js
@@ -0,0 +1,26 @@
+/*global angular */
+angular.module('app', ['ngAnimate','cgBusy'])
+  .value('cgBusyDefaults', {
+    backdrop: true,
+    delay: 300,
+    message: 'Please wait...',
+    templateUrl: 'custom-cgbusy-template.html',
+    wrapperClass: 'cg-busy cg-busy-animation fixed-center'
+  });
+
+angular.module('app').controller('DemoCtrl',function($scope,$http){
+
+	$scope.delay = 0;
+	$scope.minDuration = 0;
+	$scope.message = 'Please Wait...';
+	$scope.backdrop = true;
+	$scope.promise = null;
+
+	$scope.demo = function(){
+
+		$scope.promise = $http.get('http://httpbin.org/delay/3');
+
+	};
+
+
+});
